42f62caa4b78f6e3a2fb48dcb3b58d13b111ef37
Julian <230011@o365.htl-leoben.at>
2026-07-07 15:56:24 +0200

    Start flutter frontend

---
+++ frontend/lib/models/task.dart
@@ -0,0 +1,26 @@
+class Task {
+  final String id;
+  final String title;
+  final String description;
+
+  Task({
+  required this.id,
+  required this.title,
+  required this.description
+});
+
+  factory Task.fromJson(Map<String, dynamic> json) {
+    return Task(
+      id: json['id'],
+      title: json['title'],
+      description: json['description'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'title': title,
+      'description': description,
+    };
+  }
+}
